e9daca64d3d6fab5d3a89b577b7ee9eb9d8c86c9
HelloProgramme <343363623@qq.com>
2023-05-17 14:56:40 +0800

    框架搭建

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 引入重置文件
+import "normalize.css"
+import "@/assets/css/reset.css"
+// 引入样式
+import "@/assets/css/index.less"
+
+// 引入路由
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+// 引入store
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Suspense fallback="loading">
+    <Provider store={store}>
+      <HashRouter>
+        <App></App>
+      </HashRouter>
+    </Provider>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Suspense>
+
+
+)
